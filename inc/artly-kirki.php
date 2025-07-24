82332ad7ba0ae47575e76e410f88aff4ab3b662d
Leon <hogaifauddinbhuiyan100@gmail.com>
2025-07-24 09:47:34 +0600

    today test

--- inc/artly-kirki.php
+++ inc/artly-kirki.php
@@ -141,65 +141,65 @@ function artly_offcanvas_section() {
 artly_offcanvas_section();
 
 
-// Footer Section
-function footer_logo(){
+// // Footer Section
+// function footer_logo(){
 
-  new \Kirki\Section(
-    'footer_section',
-    [
-      'title'       => esc_html__( 'Footer', 'kirki' ),
-      'description' => esc_html__( 'My Section Description.', 'kirki' ),
-      'panel'       => 'artly_theme_options',
-      'priority'    => 160,
-    ]
-  );
+//   new \Kirki\Section(
+//     'footer_section',
+//     [
+//       'title'       => esc_html__( 'Footer', 'kirki' ),
+//       'description' => esc_html__( 'My Section Description.', 'kirki' ),
+//       'panel'       => 'artly_theme_options',
+//       'priority'    => 160,
+//     ]
+//   );
   
-  new \Kirki\Field\Image(
-    [
-      'settings'    => 'footer_logo',
-      'label'       => esc_html__( 'Footer Logo', 'kirki' ),
-      'description' => esc_html__( 'Upload Your footer Logo...', 'kirki' ),
-      'section'     => 'footer_section',
-      'default'     => get_template_directory_uri().'/assets/img/logo/logo-white.png',
-    ]
-  );
+//   new \Kirki\Field\Image(
+//     [
+//       'settings'    => 'footer_logo',
+//       'label'       => esc_html__( 'Footer Logo', 'kirki' ),
+//       'description' => esc_html__( 'Upload Your footer Logo...', 'kirki' ),
+//       'section'     => 'footer_section',
+//       'default'     => get_template_directory_uri().'/assets/img/logo/logo-white.png',
+//     ]
+//   );
 
-  new \Kirki\Field\Checkbox_Switch(
-    [
-      'settings'    => 'footer_switch',
-      'label'       => esc_html__( 'Top Footer Switch', 'kirki' ),
-      'description' => esc_html__( 'Simple switch control', 'kirki' ),
-      'section'     => 'footer_section',
-      'default'     => 'off',
-      'choices'     => [
-        'on'  => esc_html__( 'Enable', 'kirki' ),
-        'off' => esc_html__( 'Disable', 'kirki' ),
-      ],
-    ]
-  );
+//   new \Kirki\Field\Checkbox_Switch(
+//     [
+//       'settings'    => 'footer_switch',
+//       'label'       => esc_html__( 'Top Footer Switch', 'kirki' ),
+//       'description' => esc_html__( 'Simple switch control', 'kirki' ),
+//       'section'     => 'footer_section',
+//       'default'     => 'off',
+//       'choices'     => [
+//         'on'  => esc_html__( 'Enable', 'kirki' ),
+//         'off' => esc_html__( 'Disable', 'kirki' ),
+//       ],
+//     ]
+//   );
 
-  new \Kirki\Field\Checkbox_Switch(
-    [
-      'settings'    => 'footer_shape_switch',
-      'label'       => esc_html__( 'Footer Shape Switch', 'kirki' ),
-      'description' => esc_html__( 'Simple switch control', 'kirki' ),
-      'section'     => 'footer_section',
-      'default'     => 'off',
-      'choices'     => [
-        'on'  => esc_html__( 'Enable', 'kirki' ),
-        'off' => esc_html__( 'Disable', 'kirki' ),
-      ],
-    ]
-  );
+//   new \Kirki\Field\Checkbox_Switch(
+//     [
+//       'settings'    => 'footer_shape_switch',
+//       'label'       => esc_html__( 'Footer Shape Switch', 'kirki' ),
+//       'description' => esc_html__( 'Simple switch control', 'kirki' ),
+//       'section'     => 'footer_section',
+//       'default'     => 'off',
+//       'choices'     => [
+//         'on'  => esc_html__( 'Enable', 'kirki' ),
+//         'off' => esc_html__( 'Disable', 'kirki' ),
+//       ],
+//     ]
+//   );
 
-  new \Kirki\Field\Text(
-    [
-      'settings' => 'footer_copyright',
-      'label'    => esc_html__( 'Copyright', 'kirki' ),
-      'section'  => 'footer_section',
-      'default'  => esc_html__( '© Copyright 2024 | Alright reserved exdos by Themepure ', 'kirki' ),
-      'priority' => 10,
-    ]
-  );
-}
-footer_logo(); 
+//   new \Kirki\Field\Text(
+//     [
+//       'settings' => 'footer_copyright',
+//       'label'    => esc_html__( 'Copyright', 'kirki' ),
+//       'section'  => 'footer_section',
+//       'default'  => esc_html__( '© Copyright 2024 | Alright reserved exdos by Themepure ', 'kirki' ),
+//       'priority' => 10,
+//     ]
+//   );
+// }
+// footer_logo(); 
